51fe05807c0958a3897af306ae53c6622ff1993b
Agent_RBY_ <mrget112@gmail.com>
2022-01-02 13:33:53 +0300

    Create onw interface
    Took 2 minutes

--- environment.d.ts
+++ environment.d.ts
@@ -1,17 +1,17 @@
 /* eslint-disable unicorn/prevent-abbreviations */
 
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      botToken: string;
-      environment: 'dev' | 'prod' | 'debug';
-      prefix: string;
-      ownersID: string;
-      sauceNAOToken: string;
-      mongoURI: string;
-      mode: 'production' | 'testing' | 'development';
-    }
-  }
+export interface Environment {
+  botToken: string;
+  environment: 'dev' | 'prod' | 'debug';
+  prefix: string;
+  ownersID: string;
+  sauceNAOToken: string;
+  mongoURI: string;
+  mode: 'production' | 'testing' | 'development';
 }
 
-export {};
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends Environment {}
+  }
+}
