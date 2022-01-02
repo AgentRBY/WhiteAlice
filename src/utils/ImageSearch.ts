6554f2a3fe1ccced0eff6b8527c35f6dd12f30c7
Agent_RBY_ <mrget112@gmail.com>
2022-01-02 18:35:07 +0300

    Remove logger
    Took 8 minutes

--- src/utils/ImageSearch.ts
+++ src/utils/ImageSearch.ts
@@ -24,7 +24,6 @@ export const generateYandexSearchLink = (imageLink: string, yu: string) => {
 };
 
 export const getSitesFromYandexResponse = (response: Response): YandexImagesResponse => {
-  console.log(response.body);
   const parsedHTML = HTMLParser(JSON.parse(response.body).blocks[0].html);
 
   if (parsedHTML.querySelector('.CbirOtherSizes-EmptyMessage')) {
