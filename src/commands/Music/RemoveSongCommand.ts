ff34e6d12054958f1f1d44a2ebb68ace8522a0dc
Agent_RBY_ <mrget112@gmail.com>
2022-01-18 01:55:41 +0300

    Remove song command
    Took 29 seconds

---
+++ src/commands/Music/RemoveSongCommand.ts
@@ -0,0 +1,52 @@
+import { Command } from '../../structures/Command';
+import { client } from '../../app';
+import { ErrorEmbed, SuccessEmbed } from '../../utils/Embed';
+
+export default new Command({
+  name: 'removeSong',
+  category: 'Music',
+  aliases: ['rs'],
+  description: '',
+  examples: [],
+  usage: 'deleteSong',
+  run: async ({ message, args }) => {
+    const queue = client.disTube.getQueue(message);
+
+    if (!queue) {
+      const errorEmbed = ErrorEmbed('**Плейлист пуст**');
+      return message.reply({ embeds: [errorEmbed], allowedMentions: { repliedUser: false } });
+    }
+
+    if (queue.songs.length === 1) {
+      const embed = ErrorEmbed('**Нечего удалять**');
+      message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+      return;
+    }
+
+    const songId = Number(args[0]);
+
+    if (!songId || Number.isNaN(songId)) {
+      const embed = ErrorEmbed('**Введите айди трека**');
+      embed.setFooter({
+        text: 'Примечание: айди трека это - номер трека в плейлисте, его можно узнать прописав команду >queue',
+      });
+      message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+      return;
+    }
+
+    if (songId <= 0 || songId > queue.songs.length) {
+      const embed = ErrorEmbed('**Песни под данным айди не найдено**');
+      embed.setFooter({
+        text: 'Примечание: айди трека это - номер трека в плейлисте, его можно узнать прописав команду >queue',
+      });
+      message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+      return;
+    }
+
+    const deletedSong = queue.songs.splice(songId, 1)[0];
+
+    const embed = SuccessEmbed(`**Песня \`${deletedSong.name}\` - \`${deletedSong.uploader.name}\` удалена**`);
+    message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+    return;
+  },
+});
