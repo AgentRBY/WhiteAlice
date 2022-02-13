f1debd6955854d70929ee024327c555bb453a383
Agent_RBY_ <mrget112@gmail.com>
2022-02-13 23:33:48 +0300

    Add QueueInfo command
    Took 35 seconds

---
+++ src/commands/Music/QueueInfoCommand.ts
@@ -0,0 +1,39 @@
+import { Command } from '../../structures/Command';
+import { client } from '../../app';
+import { ErrorEmbed } from '../../utils/Embed';
+import { MessageEmbed } from 'discord.js';
+import { EmojisLinks } from '../../static/Emojis';
+import { Colors } from '../../static/Colors';
+
+export default new Command({
+  name: 'queueInfo',
+  category: 'Music',
+  aliases: [],
+  description: '',
+  examples: [],
+  usage: 'queueInfo',
+  run: async ({ message }) => {
+    const queue = client.disTube.getQueue(message);
+
+    if (!queue) {
+      const errorEmbed = ErrorEmbed('**Плейлист пуст**');
+      return message.reply({ embeds: [errorEmbed], allowedMentions: { repliedUser: false } });
+    }
+
+    const embed = new MessageEmbed()
+      .setAuthor({ name: 'Плейлист', iconURL: EmojisLinks.Headphone })
+      .setColor(Colors.Green);
+
+    const repeatModes = ['Выключен', 'Повторение песни', 'Повторение плейлиста'];
+
+    const description = `
+    ➤ Количество песен в очереди: **${queue.songs.length}**
+    ➤ Время прослушивания: **${queue.formattedDuration}**
+    ➤ Текущий режим повторения: **${repeatModes[queue.repeatMode]}**
+    ➤ Громкость бота: **${queue.volume}%**`;
+
+    embed.setDescription(description);
+    message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+    return;
+  },
+});
