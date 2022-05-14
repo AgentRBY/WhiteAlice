66e1a10fa8958e3630f02c2d8bcf972cb3692b69
Agent_RBY_ <mrget112@gmail.com>
2022-05-14 22:27:54 +0300

    Add DisTube Error event
    Took 10 seconds

---
+++ src/events/DisTube/ErrorEvent.ts
@@ -0,0 +1,23 @@
+import { TextChannel } from 'discord.js';
+import { Event } from '../../structures/Event';
+import { ErrorEmbed } from '../../utils/Embed';
+
+export default new Event({
+  name: 'error',
+  type: 'distube',
+  run: (channel: TextChannel, error: Error) => {
+    const message = error.message.split('\n').pop();
+    switch (message) {
+      case 'Unknown Playlist':
+        const embed = ErrorEmbed(
+          `**Произошла ошибка:** не найден плейлист. 
+          Возможно, вы указали ссылку на приватный плейлист или в ссылке на видео содержится указание на приватный плейлист`,
+        );
+        channel.send({ embeds: [embed] });
+        return;
+      default:
+        console.log(error);
+        break;
+    }
+  },
+});
