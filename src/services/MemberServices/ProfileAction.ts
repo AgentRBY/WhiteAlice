34a5191e9969e4b85a70516542837dea9e198435
Agent_RBY_ <mrget112@gmail.com>
2022-05-22 19:01:20 +0300

    Fix message count

--- src/services/MemberServices/ProfileAction.ts
+++ src/services/MemberServices/ProfileAction.ts
@@ -2,10 +2,10 @@ import { Service } from '../../structures/Service';
 import { MemberBaseId } from '../../typings/MemberModel';
 
 export class ProfileAction {
-  async incrementMessageCount(this: Service, id: MemberBaseId): Promise<void> {
+  async incrementMessageCount(this: Service, id: MemberBaseId) {
+    console.log('test', this);
     const MemberData = await this.getMemberData(id);
 
     MemberData.messageCount++;
-    this.setMemberData(id, MemberData);
   }
 }
