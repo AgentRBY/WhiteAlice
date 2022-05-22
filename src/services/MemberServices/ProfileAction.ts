ca9a42eea80235ad05c11f90e8117962816c9a8a
Agent_RBY_ <mrget112@gmail.com>
2022-05-22 19:07:49 +0300

    Remove console.log

--- src/services/MemberServices/ProfileAction.ts
+++ src/services/MemberServices/ProfileAction.ts
@@ -3,7 +3,6 @@ import { MemberBaseId } from '../../typings/MemberModel';
 
 export class ProfileAction {
   async incrementMessageCount(this: Service, id: MemberBaseId) {
-    console.log('test', this);
     const MemberData = await this.getMemberData(id);
 
     MemberData.messageCount++;
