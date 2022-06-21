789cbece4e3adcd155c4cc0af54d2448bfd3b15a
Agent_RBY_ <mrget112@gmail.com>
2022-06-21 10:22:02 +0300

    Warn command

---
+++ src/commands/Slash/Moderation/WarnCommand.ts
@@ -0,0 +1,76 @@
+import { SlashCommand, SlashCommandRunOptions } from '../../../structures/Commands/SlashCommand';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { ErrorEmbed, SuccessEmbed } from '../../../utils/Discord/Embed';
+import { GuildMember, MessageEmbed } from 'discord.js';
+import { getMemberBaseId } from '../../../utils/Other';
+import { Emojis } from '../../../static/Emojis';
+import { Colors } from '../../../static/Colors';
+import { Warn } from '../../../typings/MemberModel';
+
+class WarnCommand extends SlashCommand {
+  meta = new SlashCommandBuilder()
+    .setName('warn')
+    .setDescription('Выдать предупреждение пользователю')
+    .addUserOption((option) => {
+      return option
+        .setName('пользователь')
+        .setDescription('Пользователь которому нужно выдать предупреждение')
+        .setRequired(true);
+    })
+    .addStringOption((option) => {
+      return option
+        .setName('причина')
+        .setDescription('Причина, из-за которой выдано предупреждение')
+        .setRequired(false);
+    });
+
+  async run({ client, interaction }: SlashCommandRunOptions) {
+    if (!interaction.memberPermissions.has('BAN_MEMBERS')) {
+      const embed = ErrorEmbed('У вас нет прав на эту команду');
+      interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    const targetMember = interaction.options.getMember('пользователь', true);
+
+    if (!(targetMember instanceof GuildMember)) {
+      const embed = ErrorEmbed('Ошибка');
+      interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    const warns = await client.service.getWarns(getMemberBaseId(targetMember));
+
+    const reason = interaction.options.getString('причина', false);
+
+    const embed = SuccessEmbed(`Пользователю ${targetMember} было выдано предупреждение`).setTimestamp();
+
+    const directEmbed = new MessageEmbed()
+      .setDescription(
+        `${Emojis.Info} На сервере \`${interaction.guild}\` Вам было выдано предупреждение пользователем ${
+          interaction.user
+        }
+        Это уже ваше \`${warns.length + 1}\` предупреждение`,
+      )
+      .setColor(Colors.Red)
+      .setTimestamp();
+
+    if (reason) {
+      embed.setFooter({ text: `Причина: ${reason}` });
+      directEmbed.setFooter({ text: `Причина: ${reason}` });
+    }
+
+    const warn: Warn = {
+      date: Date.now(),
+      reason: reason,
+      givenBy: interaction.user.id,
+    };
+
+    client.service.addWarn(getMemberBaseId(targetMember), warn);
+
+    interaction.reply({ embeds: [embed] });
+    targetMember.send({ embeds: [directEmbed] });
+  }
+}
+
+export default new WarnCommand();
