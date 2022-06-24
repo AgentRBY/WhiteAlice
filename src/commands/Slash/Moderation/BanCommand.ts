a19f8854b68a3ea3d10806c18a9baf2e9ba434c9
Agent_RBY_ <mrget112@gmail.com>
2022-06-24 16:08:15 +0300

    Ban and unban slash-command

---
+++ src/commands/Slash/Moderation/BanCommand.ts
@@ -0,0 +1,98 @@
+import { SlashCommand, SlashCommandRunOptions } from '../../../structures/Commands/SlashCommand';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { ErrorEmbed, SuccessEmbed } from '../../../utils/Discord/Embed';
+import { GuildMember, MessageEmbed } from 'discord.js';
+import { Colors } from '../../../static/Colors';
+import { Emojis } from '../../../static/Emojis';
+import { Ban } from '../../../typings/MemberModel';
+import { client } from '../../../app';
+
+class BanCommand extends SlashCommand {
+  meta = new SlashCommandBuilder()
+    .setName('ban')
+    .setDescription('Забанить пользователя')
+    .addUserOption((option) => {
+      return option
+        .setName('пользователь')
+        .setDescription('Пользователь, которого вы хотите забанить')
+        .setRequired(true);
+    })
+    .addStringOption((option) => {
+      return option.setName('причина').setDescription('Причина бана').setRequired(false);
+    });
+
+  async run({ interaction }: SlashCommandRunOptions) {
+    if (!interaction.memberPermissions.has('BAN_MEMBERS')) {
+      const embed = ErrorEmbed('У вас нет прав на эту команду');
+      interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    const targetMember = interaction.options.getMember('пользователь', true);
+
+    if (!(targetMember instanceof GuildMember)) {
+      const embed = ErrorEmbed('Ошибка');
+      interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    await interaction.guild.bans.fetch();
+
+    if (interaction.guild.bans.cache.get(targetMember.id)) {
+      const embed = ErrorEmbed('Пользователь уже в бане');
+      interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    if (!targetMember.bannable) {
+      const embed = ErrorEmbed('У меня нет прав, что бы забанить этого пользователя');
+      interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    if (!targetMember.bannable) {
+      const embed = ErrorEmbed('У меня нет прав, что бы забанить этого пользователя');
+      interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    if (targetMember.roles.highest.comparePositionTo(interaction.guild.me.roles.highest) >= 0) {
+      const embed = ErrorEmbed('У вас нет прав, что бы замутить этого пользователя');
+      interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    const reason = interaction.options.getString('причина', false);
+
+    const embed = SuccessEmbed(`Пользователь ${targetMember} был забанен`);
+    const directEmbed = new MessageEmbed()
+      .setColor(Colors.Red)
+      .setDescription(
+        `${Emojis.Info} На сервере \`${targetMember.guild}\` Вы получили бан от пользователя ${interaction.user}`,
+      )
+      .setTimestamp();
+
+    if (reason) {
+      embed.setFooter({ text: `Причина: ${reason}` });
+      directEmbed.setFooter({ text: `Причина: ${reason}` });
+    }
+
+    const ban: Ban = {
+      date: Date.now(),
+      reason,
+      givenBy: client.user.id,
+      messageDeleteCountInDays: 0,
+    };
+
+    client.service.addBan(`${targetMember.id}-${targetMember.guild.id}`, ban);
+
+    await targetMember.send({ embeds: [directEmbed] }).catch(() => {});
+    interaction.reply({ embeds: [embed] });
+
+    interaction.guild.bans.create(targetMember.id, {
+      reason,
+    });
+  }
+}
+
+export default new BanCommand();
---
+++ src/commands/Slash/Moderation/UnbanCommand.ts
@@ -0,0 +1,99 @@
+import {
+  AutocompleteRunOptions,
+  SlashCommand,
+  SlashCommandRunOptions,
+} from '../../../structures/Commands/SlashCommand';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Searcher } from 'fast-fuzzy';
+import { ErrorEmbed, SuccessEmbed } from '../../../utils/Discord/Embed';
+import { MessageEmbed } from 'discord.js';
+import { Colors } from '../../../static/Colors';
+import { Emojis } from '../../../static/Emojis';
+
+class UnbanCommand extends SlashCommand {
+  meta = new SlashCommandBuilder()
+    .setName('unban')
+    .setDescription('Разбанить пользователя')
+    .addStringOption((option) => {
+      return option
+        .setName('пользователь')
+        .setDescription('Пользователь, которого вы хотите разбанить')
+        .setRequired(true)
+        .setAutocomplete(true);
+    })
+    .addStringOption((option) => {
+      return option.setName('причина').setDescription('Причина разбана').setRequired(false);
+    });
+
+  async run({ client, interaction }: SlashCommandRunOptions) {
+    if (!interaction.memberPermissions.has('BAN_MEMBERS')) {
+      const embed = ErrorEmbed('У вас нет прав на эту команду');
+      interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    const targetUserId = interaction.options.getString('пользователь', true);
+    const user = await client.users.fetch(targetUserId).catch(() => {});
+
+    if (!user) {
+      const embed = ErrorEmbed('Пользователь не найден');
+      embed.setFooter({ text: 'Возможно, он удалил аккаунт или его забанил сам Discord' });
+      interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    const reason = interaction.options.getString('причина', false);
+
+    interaction.guild.bans.remove(targetUserId, reason);
+
+    const embed = SuccessEmbed(`Пользователь ${user} был разбанен`);
+    const directEmbed = new MessageEmbed()
+      .setColor(Colors.Blue)
+      .setDescription(
+        `${Emojis.Info} На сервере \`${interaction.guild}\` Вас разбанил пользователь ${interaction.user}`,
+      )
+      .setTimestamp();
+
+    client.service.removeBan(`${targetUserId}-${interaction.guild.id}`, interaction.user.id, reason);
+
+    if (reason) {
+      embed.setFooter({ text: `Причина разбана: ${reason}` });
+      directEmbed.setFooter({ text: `Причина разбана: ${reason}` });
+    }
+
+    user.send({ embeds: [directEmbed] }).catch(() => {});
+    interaction.reply({ embeds: [embed] });
+  }
+
+  async handleAutocomplete({ interaction }: AutocompleteRunOptions) {
+    const focusedValue = interaction.options.getFocused(true);
+
+    if (focusedValue.name !== 'пользователь') {
+      return;
+    }
+
+    const bans = await interaction.guild.bans.fetch();
+
+    const mappedBans = bans.map((ban) => {
+      return {
+        name: `@${ban.user.tag}`,
+        value: ban.user.id,
+      };
+    });
+
+    if (focusedValue.value === '') {
+      await interaction.respond(mappedBans);
+      return;
+    }
+
+    const searcher = new Searcher(mappedBans, {
+      keySelector: (item) => item.name,
+    });
+
+    const filteredBans = searcher.search(focusedValue.value as string);
+
+    await interaction.respond(filteredBans);
+  }
+}
+
+export default new UnbanCommand();
