efddd373f94ad8aa231d0dcf51200d47d339b046
Agent_RBY_ <mrget112@gmail.com>
2022-07-25 12:19:26 +0300

    Voice fix again

--- src/events/Guild/CustomVoices/CustomVoiceAuthorLeaveEvent.ts
+++ src/events/Guild/CustomVoices/CustomVoiceAuthorLeaveEvent.ts
@@ -9,7 +9,7 @@ class CustomVoiceAuthorLeave extends DiscordEvent<'voiceStateUpdate'> {
   name: DiscordEventNames = 'voiceStateUpdate';
 
   async run(client: ExtendClient, oldState: VoiceState, newState: VoiceState) {
-    if (newState || oldState.channel === newState.channel) {
+    if (oldState.channel === newState?.channel) {
       return;
     }
 
