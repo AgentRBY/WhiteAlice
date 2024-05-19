e3a9a10bc14e9e82d0fd52b9076cb5943f9917f2
Agent_RBY_ <mrget112@gmail.com>
2024-05-19 12:42:52 +0300

    Fix font

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import { ExtendClient } from './structures/Client';
 import Logger from './utils/Logger';
 import durationPlugin from 'dayjs/plugin/duration';
 import dayjs from 'dayjs';
+import { GlobalFonts } from '@napi-rs/canvas';
+import path from 'node:path';
 
 dayjs.extend(durationPlugin);
 
 require('dotenv').config();
+
+GlobalFonts.registerFromPath(path.join(__dirname, 'utils', 'Canvas', 'Font', 'SegoeUI.ttf'), 'Segoe UI');
 
 export const client = new ExtendClient();
 
