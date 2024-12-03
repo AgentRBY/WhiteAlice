d851302e903fa63ae1a97093e2aa869bc107f7e8
Agent_RBY_ <mrget112@gmail.com>
2024-12-04 00:13:19 +0200

    Add emoji support

--- src/commands/Common/Administration/StealEmojiCommand.ts
+++ src/commands/Common/Administration/StealEmojiCommand.ts
@@ -1,31 +1,52 @@
 import { CommandExample, CommandRunOptions, CommonCommand } from '../../../structures/Commands/CommonCommand';
-import { isDiscordEmojiLink, removeQueryParameters } from '../../../utils/Common/Strings';
+import { isDiscordEmojiLink, isLink, removeQueryParameters } from '../../../utils/Common/Strings';
 import { DiscordAPIError, PermissionString, SnowflakeUtil } from 'discord.js';
 
 class StealEmoji extends CommonCommand {
   name = 'stealEmoji';
   category = 'Administration';
   aliases = [];
-  description = '';
-  examples: CommandExample[] = [];
-  usage = 'stealEmoji';
+  description = 'Добавить эмодзи с чужого сервера или по ссылке';
+  examples: CommandExample[] = [
+    {
+      command: 'stealEmoji :emoji:',
+      description: 'Добавить эмодзи с чужого сервера',
+    },
+    {
+      command: 'stealEmoji https://cdn.discordapp.com/emojis/123456789.png',
+      description: 'Добавить эмодзи с ссылки на эмодзи',
+    },
+  ];
+  usage = 'stealEmoji <ссылка | эмодзи>';
   botPermissions: PermissionString[] = ['MANAGE_EMOJIS_AND_STICKERS'];
   memberPermissions: PermissionString[] = ['MANAGE_EMOJIS_AND_STICKERS'];
 
   async run({ message, args }: CommandRunOptions) {
-    const link = args[0];
+    const linkOrEmoji = args[0];
 
-    if (!link) {
-      message.sendError('**Введите ссылку на эмодзи**');
+    if (!linkOrEmoji) {
+      message.sendError('**Введите ссылку на эмодзи или сам эмодзи**');
       return;
     }
 
-    if (!isDiscordEmojiLink(link)) {
+    const isEmojiLink = isLink(linkOrEmoji);
+
+    if (isEmojiLink && !isDiscordEmojiLink(linkOrEmoji)) {
       message.sendError('**Ссылка не является эмоджи**');
       return;
     }
 
-    const name = args[1] || SnowflakeUtil.generate();
+    const link = isEmojiLink ? linkOrEmoji : this.generateEmojiLink(linkOrEmoji);
+
+    const name = args[1] || (isEmojiLink ? SnowflakeUtil.generate() : this.getEmojiName(linkOrEmoji));
+
+    if (name.length > 32) {
+      return message.sendError('**Имя эмодзи не должно превышать 32 символа**');
+    }
+
+    if (message.guild.emojis.cache.some((emoji) => emoji.name === name)) {
+      return message.sendError('**Эмодзи с таким именем уже существует на сервере**');
+    }
 
     message.guild.emojis
       .create(this.getHighQualityEmoji(link), name)
@@ -33,6 +54,7 @@ class StealEmoji extends CommonCommand {
         message.sendSuccess(`**Эмодзи ${emoji.toString()} (\`:${emoji.name}:\`) успешно добавлено**`);
       })
       .catch((error) => {
+        console.log(error);
         if (error instanceof DiscordAPIError && error.message.includes('Maximum number of emojis reached')) {
           return message.sendError('**Не удалось добавить эмодзи, все свободные слоты заняты**');
         }
@@ -42,7 +64,7 @@ class StealEmoji extends CommonCommand {
   }
 
   private getHighQualityEmoji(link: string) {
-    const query = new URLSearchParams(link);
+    const query = new URLSearchParams(link.split('?')?.[1]);
 
     query.set('size', '4096');
     query.set('quality', 'lossless');
@@ -54,6 +76,16 @@ class StealEmoji extends CommonCommand {
     }
 
     return baseLink + '?' + query.toString();
+  }
+
+  private generateEmojiLink(emoji: string) {
+    const emojiId = emoji.split(':')[2].replace('>', '');
+
+    return `https://cdn.discordapp.com/emojis/${emojiId}.png`;
+  }
+
+  private getEmojiName(emoji: string) {
+    return emoji.split(':')[1];
   }
 }
 
